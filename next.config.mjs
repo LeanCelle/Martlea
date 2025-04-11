635d5166ed59996f339ef4d417366cacc015775f
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-04-10 23:17:55 -0300

    Modify data configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
+import 'dotenv/config';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    env: {
-      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    },
-  };
-  
-  export default nextConfig;
-  
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+};
+
+export default nextConfig;
