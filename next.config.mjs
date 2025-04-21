3f7439b108a7aadb9a9eeecc8fef699a46eaf813
Leandro Celle (WPP Group USA LLC) <leangerman13@gmail.com>
2025-04-21 03:48:37 -0300

    Adding security policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
+  // ...
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          /*{
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: blob: https:;
+              font-src 'self' https:;
+              connect-src *;
+              object-src 'none';
+              frame-ancestors 'none';
+            `.replace(/\n/g, '')
+          },*/
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer-when-downgrade'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=()'
+          },
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
